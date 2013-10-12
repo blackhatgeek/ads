651280fffcecbac8edcfa9de9deea49a1581917a
Alexander Mansurov <alexander.mansurov@gmail.com>
2013-10-12 14:12:58 +0200

    soucet dvou cisel podle algoritmu z prednasky

--- p1x/longint.pas
+++ p1x/longint.pas
@@ -1,8 +1,14 @@
 program longint;
 
-type cislo=0..9;
-type longint_32=array[1..10] of cislo;
-type longint_32a=array[0..10] of cislo;{kontrola preteceni}
+type 
+cislice=0..9;
+longint_32=array[1..10] of cislice;
+longint_32a=array[0..10] of cislice;{kontrola preteceni}
+zaznam=record c:cislice; p:boolean end;
+
+var 
+bez:array[cislice,cislice] of zaznam; 
+s:array[cislice,cislice] of zaznam;
 
 function secti(longint_32 a, longint_32 b):longint_32a;
 var soucet:longint_32a;
@@ -16,6 +22,28 @@ begin
 		end else soucet[i]:=a[i]+b[i];
 	if soucet[1]>9 then soucet[0]:=1 else soucet[0]:=0;
 	secti:=soucet;
+end;
+
+function f_soucet(longint_32 a, longint_32 b):longint_32a;
+var aa,bb,cc:longint_32a; i,j,delka_a,delka_b:integer;pom:longint_32; stav:boolean;
+begin
+	{ktere z cisel je delsi - najdu prvni nenulovou cislici a a prvni nenulovou cislici b}
+	delka_a:=0;i:=0;while a[i]<>0 do i:=i+1; delka_a:=10-i;
+	delka_b:=0;i:=0;while b[i]<>0 do i:=i+1; delka_b:=10-i;
+	{delsi z cisel} if delka_a<delka_b then begin pom:=a; a:=b; b:=a; i:=delka_a;delka_a:=delka_b;delka_b:=i end;
+	{doplnime z leva jednou nulou} aa[0]:=0; for i:=1 to 10 do aa[i]:=a[i];
+	{kratsi z cisel doplnime z leva tolika nulami, aby byla stejne dlouha}
+	bb[0]:=0; for i:=1 to (delka_a-delka_b-1) do bb[i]:=0;
+	{postupujeme zprava a ke kazde dvojici vysledku urcime cislici vysledku z prislusneho pole dle stavu}
+	{na zacatku je stav bez prenosu}stav:=false;
+	for i:=10 downto 0 do
+		if stav=false then begin
+			cc[i]:=bez[aa[i],bb[i]].c;
+			stav:=bez[aa[i],bb[i]].p;	
+		end else begin
+			cc[i]:=s[aa[i],bb[i]].c;
+			stav:=s[aa[i],bb[i]].p;
+		end;
 end;
 
 function vetsi(longint_32 a,longint_32 b,var stejne:boolean):boolean;
@@ -73,3 +101,29 @@ begin
 	rozdil[1]:=a[1]-b[1];{nastavi se minus a spravny rozdil}
 	odecti:=rozdil;
 end;
+
+begin
+	
+	{naplnim pole pro stav bez prenosu}
+	for i:=0 to 9 do begin {radek, y}
+		for j:=0 to (9-i) do begin{sloupec, x}
+			bez[j,i].c:=i+j;
+			bez[j,i].p:=false;
+		end;
+		for j:=(9-i+1) to 9 do begin
+			bez[j,i].c:=10-i-j;
+			bez[j,i].p:=true;
+		end;
+	end;
+	{naplnim pole pro stav s prenosem}
+	for j:=0 to 9 do begin{sloupec,x}
+		for i:=0 to (8-j) do begin{radek,y}
+			s[j,i].c:=i+j+1;
+			s[j,i].p:=false;
+		end;
+		for i:=(9-j) to 9 do begin
+			s[j,i].c:=9-i-j;
+			s[j,i].p:=true;
+		end;
+	end;
+end.
