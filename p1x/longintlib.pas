9645c33fbcc607b8e8510dca832a6cf313678804
Alexander Mansurov <alexander.mansurov@gmail.com>
2013-10-12 22:59:20 +0200

    Castecny fix tabulky rozdil

--- p1x/longintlib.pas
+++ p1x/longintlib.pas
@@ -9,7 +9,7 @@ zaznam=record c:cislice; p:boolean end;
 
 var 
 souc_bez,souc_s,rozd_bez,rozd_s:array[cislice,cislice] of zaznam; 
-i,j:integer;
+i,j,k:integer;
 q:longint_32a;
 r:longint_32b;
 
@@ -54,6 +54,8 @@ end;
 function f_rozdil(a,b:longint_32):longint_32b;
 var aa,bb,cc:longint_32a;delka_a,delka_b:integer;pom:longint_32;stav:boolean;vysl:longint_32b;
 begin
+	print0(a);
+	print0(b);
 	{ktere z cisel je delsi - najdu prvni nenulovou cislici a a prvni nenulovou cislici b}
 	delka_a:=0;i:=0;while a[i]<>0 do i:=i+1; delka_a:=10-i;
 	delka_b:=0;i:=0;while b[i]<>0 do i:=i+1; delka_b:=10-i;
@@ -123,11 +125,6 @@ begin
 			souc_bez[j,i].p:=true;
 		end;
 	end;
-	for i:=0 to 9 do begin
-		for j:=0 to 9 do if souc_bez[j,i].p then write('[',souc_bez[j,i].c,'] ') else write(souc_bez[j,i].c,' ');
-		writeln('');
-	end;
-	writeln('');
 	{naplnim pole pro stav s prenosem}
 	for j:=0 to 9 do begin{sloupec,x}
 		for i:=0 to (8-j) do begin{radek,y}
@@ -140,34 +137,53 @@ begin
 		end;
 	end;
 	souc_s[9,9].c:=0;
-	for i:=0 to 9 do begin
-		for j:=0 to 9 do if souc_s[j,i].p then write('[',souc_s[j,i].c,'] ') else write(souc_s[j,i].c,' ');
-		writeln('');
-	end;
+	
 	{ODCITANI}
 	{naplnim pole pro stav bez prenosu}
 	for i:=0 to 9 do begin{radek, mensitel}
+		k:=0;
 		for j:=i to 9 do begin{sloupec, mensenec}
-			rozd_bez[j,i].c:=j-i;
+			rozd_bez[j,i].c:=k;
 			rozd_bez[j,i].p:=false;
+			k:=k+1;
 		end;
-		for j:=1 to (i-1) do begin
-			rozd_bez[j,i].c:=i+j;
+		for j:=0 to (i-1) do begin
+			rozd_bez[j,i].c:=k;
 			rozd_bez[j,i].p:=true;
+			k:=k+1;
 		end;
 	end;
+	for i:=0 to 9 do begin
+		for j:=0 to 9 do if rozd_bez[j,i].p then write('[',rozd_bez[j,i].c,'] ') else write(rozd_bez[j,i].c,' ');
+		writeln('');
+	end;
+	writeln('');
 	{naplnim pole pro stav s prenosem}
 	rozd_s[0,0].c:=9;rozd_s[0,0].p:=false;
-	for j:=1 to 8 do {mensenec}
-		for i:=(j+1) to 9 do begin{mensitel}
-			rozd_s[j,i].c:=i+1-j;
-			rozd_s[j,i].p:=false;
-		end;
+	k:=0;
+	for i:=1 to 9 do begin
+		rozd_s[i,0].c:=k;
+		rozd_s[i,0].p:=false;
+		k:=k+1;
+	end;
 	for i:=1 to 9 do{mensitel}
 		for j:=0 to i do begin{mensenec}
 			rozd_s[j,i].c:=j+9-i;
 			rozd_s[j,i].p:=true;
 		end;
+	for i:=1 to 9 do begin{mensenec}
+		k:=0;
+		for j:=(i+1) to 10 do begin{mensitel}
+			rozd_s[j,i].c:=k;
+			rozd_s[j,i].p:=false;
+			k:=k+1;
+		end;
+	end;
+	for i:=0 to 9 do begin
+		for j:=0 to 9 do if rozd_s[j,i].p then write('[',rozd_s[j,i].c,'] ') else write(rozd_s[j,i].c,' ');
+		writeln('');
+	end;
+	writeln('');
 	q:=f_soucet(convert0(222222222),convert0(222222222));
 	r:=f_rozdil(convert0(222222222),convert0(222222222));
 	print0(convert3(r.cislo));
