18ae3d537410f573ba99a7da6b242a13659aeeab
Alexander Mansurov <alexander.mansurov@gmail.com>
2013-10-22 19:46:19 +0200

    Nasobeni -1

--- p1x/postfix.pas
+++ p1x/postfix.pas
@@ -234,18 +234,23 @@ begin
 					O:=f_rozdil(O1,O2);
 				end;
 				'*': begin{zde q je pomocna promenna, kterou pouzivame pri porovnani na rovnost}
+					writeln('Nasobim:');
 					O:=nula;
 					if vetsi(O2,O1,q) then begin
-						writeln('Prohazuji O2 a O1');
 						O:=O2;
 						O2:=O1;
 						O1:=O;
 						O:=nula;
 					end;{pricitam mensi cislo}
+					print0(O1); print0(O2);
 					vetsi(O2,nula,q);
-					if q{O2='nula'} then begin O:=nula; writeln('O2 = 0') end else begin
+					if q{O2='nula'} then O:=nula else begin
 						vetsi(O2,jedna,q);
-						if q{O2=1} then begin O:=O1; writeln('O2 = 1') end
+						jedna.znamenko:=false;
+						vetsi(O2,jedna,p);
+						jedna.znamenko:=false;
+						if p{O2=-1} then begin O:=O1; O.znamenko:=false end 
+						else if q{O2=1} then O:=O1
 						else begin
 							soucet:=nula;
 							print0(soucet);
@@ -257,9 +262,12 @@ begin
 								print0(O2);
 								soucet:=f_soucet(soucet,O2);
 							end;
+							if O2.znamenko=O1.znamenko then soucet.znamenko:=true else soucet.znamenko:=false;
+							write (O1.znamenko,O2.znamenko,soucet.znamenko);
 							O:=soucet;
 						end
-					end
+					end;
+					print0(O);
 				end;
 				'/': begin
 					vetsi(O2,nula,q);
