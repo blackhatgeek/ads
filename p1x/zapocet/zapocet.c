a698b1a174eb882b4d79ab9c8db2e1d738992936
Alexander Mansurov <alexander.mansurov@gmail.com>
2013-11-13 18:25:50 +0100

    predelany navrh

--- p1x/zapocet/zapocet.c
+++ p1x/zapocet/zapocet.c
@@ -1,41 +1,74 @@
-#include<dirent.h>
-#include<sys/types.h>
-#include<sys/stat.h>
 #include<stdio.h>
-#include<string.h>
+#include<dirent.h>
 
-DIR *dir;
-char dir_uri;
+#define TRUE 1
+#define FALSE 0
+#define T_FILE 'f'
+#define T_DIR 'd'
+#define STACK_MAX 10
 
-int otevri_adresar(){
-	dir=opendir(&dir_uri);//pokus o otevreni adresare
-	if(dir==NULL){
-		fprintf(stderr,"Chyba pri vstupu do adresare: %s\n",&dir_uri);
-		return 1;//fail
-	}else return 0;
+//zasobnik konstantni delky
+struct T_STACK{
+	char *stack[STACK_MAX];
+	unsigned short head;
+} stack;
+
+//vlozi do zasobniku adresar
+int stack_push(char *dir){
+	stack.head++;
+	if (stack.head<STACK_MAX){
+		stack.stack[stack.head]=dir;
+		return TRUE;
+	}else return FALSE;
 }
 
-int vypis_adresar(){
-	//otevreli jsme adresar - vypis obsazenych souboru vc. velikosti
-	struct dirent *de=readdir(dir);
-	struct stat st;
-	while(de!=NULL){
-		if (!((strlen(de->d_name)==1) && (de->d_name[0]=='.')) && !((strlen(de->d_name)==2) && (de->d_name[0]=='.') && (de->d_name[1]=='.'))){//nevypisuji . a ..
-			//if(de->d_type==DT_DIR) fprintf(stdout,"d\n");
-			if(de->d_type==DT_REG){
-//				char *path=strcat(strcat(&dir_uri,"/"),de->d_name);
-//				fprintf(stdout,"%s\n",path);
-//				stat(dir_uri."/".*de->d_name,&st);
-				fprintf(stdout,"%s\n",de->d_name);
-			}
-		}
-		de=readdir(dir);
-	}		
+//vezmi adresar ze zasobniku a pokus se ho otevrit - vrati ukazatel na soubor
+int stack_pop_open(FILE *file){
+	if ((stack.head>-1)&&(stack.head<STACK_MAX)){
+		DIR *dir=opendir(stack.stack[stack.head]);//vezme ze zasobniku adresu adresare a pokusi se ho otevrit
+		stack.head--;
+//		struct dirent *de=readdir(stack.stack[stack.head]);
+		
+		
+	} else{
+		file=NULL;
+		return FALSE;
+	}
 }
+
+//cti prvek v adresari - dostane adresar, vrati jmeno souboru
+int read_dir(FILE *dir, char *name){
+}
+
+//vlozi do "databaze" jmeno souboru a velikost v B
+int db_put(char *name, int length){
+}
+
+//vrati delku souboru - dostane soubor a jmeno, vrati delku v B, pokud soubor neexistuje, je to 0
+int get_length(FILE *dir,char *name){
+}
+
 
 int main(){
-	fscanf(stdin,"%s",&dir_uri);//nacte vstupni adresu
-	if(otevri_adresar()==1) return 1;
-	else vypis_adresar();
-	return 0;
+	char *dir,*name;
+	FILE file;
+	stack.head=-1;
+
+	//nacti vstupni adresar
+	fscanf(stdin,"%s",dir);
+
+	//vloz adresar do zasobniku
+	stack_push(dir);
+
+	//dokud je zasobnik neprazdny
+	while(stack.head>-1){
+		//vezmi adresar ze zasobniku a pokus se ho otevrit
+		if(stack_pop_open(&file)==TRUE){
+			//pokud se povedlo adresar otevri, cti prvky v adresari
+			//je-li to soubor, vloz ho do "databaze": dle velikosti usporadane skupiny souboru, vsechny prvky nejake skupiny maji stejnou velikost
+			//je-li to adresar, vloz ho do zasobniku
+			if(read_dir(&file,name)==T_FILE) db_put(name,get_length(&file,name));
+			else stack_push(name);
+		}
+	}
 }
